aa98836e9cd96c5c3f9979b9a8ec8e8122b843b5
Harris Williams <60046112+graderthan@users.noreply.github.com>
2023-11-19 15:59:24 -0500

    Fixed typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Python interface for Vex v5',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/vex-v5-python-interface',
+    url='https://github.com/graderthan/vex-v5-python-interface',
     author='Harris Williams',
     author_email='support@GraderThan.com',
     license='Grader Than LLC Academic Content License',
